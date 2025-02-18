4b9b208b5f7d7bc881f144f9c947a4b2c47cf1c0
NewMoon <149091994+rakazabra@users.noreply.github.com>
2025-02-18 10:47:10 -0500

    products section created

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,15 +1,2 @@
-
-       
-  
-        for(let i = 0; i < 8; i++) {
-
-           document.body.append(sliderDot);
-        };
-        sliderDot.className = "slider__dot";
-
-
-        sliderDot.addEventListener("click", () => {
-            sliderDot.classList.add("active");
-            alert("23e")
-        });
+const dot = document.getElementsByClassName("dot");
 
